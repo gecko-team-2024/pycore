44937f28170ae8b387cb6d56f228b75db2771b3c
PM <geckoteam2024@gmail.com>
2025-03-30 15:34:18 +0700

    update v2.2

--- routes/routes.go
+++ routes/routes.go
@@ -1,6 +1,7 @@
 package routes
 
 import (
+	"net/http"
 	"pycore/controllers"
 
 	"github.com/gorilla/mux"
@@ -11,6 +12,9 @@ func UserHandleRoutes() *mux.Router {
 
 	router.HandleFunc("/api/v1/register", controllers.RegisterHandler).Methods("POST")
 	router.HandleFunc("/api/v1/login", controllers.LoginHandler).Methods("POST")
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello, World!"))
+	}).Methods("GET")
 
 	return router
 }
