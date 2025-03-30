439514567daacb804c8267c78e256ec0227b73e9
PM <geckoteam2024@gmail.com>
2025-03-30 14:16:25 +0700

    create

--- config/firebase.go
+++ config/firebase.go
@@ -3,17 +3,32 @@ package config
 import (
 	"context"
 	"log"
+	"os"
 
 	"cloud.google.com/go/firestore"
 	firebase "firebase.google.com/go"
+	"github.com/joho/godotenv"
 	"google.golang.org/api/option"
 )
 
 var Client *firestore.Client
 
 func InitFirebase() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v\n", err)
+	}
+
 	ctx := context.Background()
-	opt := option.WithCredentialsFile("serviceAccountKey.json")
+
+	// Lấy đường dẫn file credentials từ biến môi trường
+	credFile := os.Getenv("GOOGLE_APPLICATION_CREDENTIALS")
+	if credFile == "" {
+		log.Fatalf("GOOGLE_APPLICATION_CREDENTIALS is not set in the environment")
+	}
+
+	// Sử dụng file credentials từ biến môi trường
+	opt := option.WithCredentialsFile(credFile)
 	app, err := firebase.NewApp(ctx, nil, opt)
 	if err != nil {
 		log.Fatalf("Initial Firebase Error: %v\n", err)
@@ -21,7 +36,7 @@ func InitFirebase() {
 
 	Client, err = app.Firestore(ctx)
 	if err != nil {
-		log.Fatalf("Can connect Firestore: %v\n", err)
+		log.Fatalf("Cannot connect to Firestore: %v\n", err)
 	}
 
 	log.Println("Firebase initialized successfully")
--- go.mod
+++ go.mod
@@ -35,6 +35,7 @@ require (
 	github.com/google/s2a-go v0.1.9 // indirect
 	github.com/googleapis/enterprise-certificate-proxy v0.3.6 // indirect
 	github.com/googleapis/gax-go/v2 v2.14.1 // indirect
+	github.com/joho/godotenv v1.5.1
 	github.com/planetscale/vtprotobuf v0.6.1-0.20240319094008-0393e58bdf10 // indirect
 	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
 	go.opentelemetry.io/contrib/detectors/gcp v1.34.0 // indirect
